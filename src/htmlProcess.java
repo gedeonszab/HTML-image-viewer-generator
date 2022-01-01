f053070fcd534d98eb397d4d1fb84250e8af064b
Szabo Gedeon <gedeonszabo07@gmail.com>
2022-01-01 21:31:58 +0100

    Optimized

--- src/htmlProcess.java
+++ src/htmlProcess.java
@@ -1,6 +1,8 @@
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 public class htmlProcess
